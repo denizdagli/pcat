d3f688c5a717df89b7c611c3ff1deab94a212a69
denizdagli <denizdaglice@gmail.com>
2023-04-17 15:15:42 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-const fn = function () {
-  console.log('function working');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
 };
 
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware Log 2');
+  next();
+};
+//MIDDLEWARES
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
