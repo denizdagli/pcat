16501b6d347a567c08bf450302a75412bdeec81e
denizdagli <denizdaglice@gmail.com>
2023-04-24 14:22:56 +0300

    Fotograf icerik sayfasi duzenlendi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
+const fs = require('fs');
 const app = express();
 const Photo = require('./models/Photo');
 const port = 3000;
@@ -11,27 +13,26 @@ mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db', {
   useUnifiedTopology: true,
 });
 
-
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
 
 //ROUTES
 app.get('/', async (req, res) => {
-  const photos= await Photo.find({})
-  res.render('index',{
-    photos
+  const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
   });
 });
 app.get('/photos/:id', async (req, res) => {
-  
-  const photo = await Photo.findById(req.params.id)
-  res.render('photo',{
-    photo
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
   });
 });
 app.get('/about', (req, res) => {
@@ -41,11 +42,25 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 app.post('/photos', async (req, res) => {
- await Photo.create(req.body)
-  res.redirect('/')
+  //console.log(req.files.image); //görsel verilerine ulaşılır
+  //await Photo.create(req.body);
+  //res.redirect('/');
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
 });
-
-
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı.`);
