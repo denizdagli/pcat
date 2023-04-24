c9facee7cf4be57c76c82be373b62da78565bfb5
denizdagli <denizdaglice@gmail.com>
2023-04-24 16:21:43 +0300

    fotograf bilgilerini guncelleme eklendi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
 const fs = require('fs');
 const app = express();
@@ -21,7 +22,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
-
+app.use(methodOverride('_method'));
 //ROUTES
 app.get('/', async (req, res) => {
   const photos = await Photo.find({}).sort('-dateCreated');
@@ -61,7 +62,20 @@ app.post('/photos', async (req, res) => {
     res.redirect('/');
   });
 });
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title=req.body.title
+  photo.description=req.body.description
+  photo.save()
 
+  res.redirect(`/photos/${req.params.id}`)
+});
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı.`);
 });
